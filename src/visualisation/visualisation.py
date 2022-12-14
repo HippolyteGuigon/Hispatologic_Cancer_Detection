60afa783f06d9da9a4b84ab350d41152889d4539
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2022-12-14 22:05:37 +0100

    pushing_new_visualisation

--- src/visualisation/visualisation.py
+++ src/visualisation/visualisation.py
@@ -3,15 +3,17 @@ import cv2
 import os
 
 
-def image_visualisation(image="f0c2a0b8ef3024f407fa97d852d49be0215cafe0.tif") -> None:
+def image_visualisation(image="f0c2a0b8ef3024f407fa97d852d49be0215cafe0.tif",flag=1) -> None:
     """
     The goal of this function is to display a given image
     for a certain time until the user hits a command
 
     Arguments:
-        -image: The name of the image the user wants to
+        -image: str: The name of the image the user wants to
         display
-
+        -flag: int : The way the image is loaded, how colours 
+        appear
+        
     Returns:
         None
     """
@@ -21,7 +23,8 @@ def image_visualisation(image="f0c2a0b8ef3024f407fa97d852d49be0215cafe0.tif") ->
     else:
         path = os.path.join("test", image)
 
-    img = cv2.imread(path)
-    cv2.imshow("image", img)
+    img = cv2.imread(path,flag)
+    imS = cv2.resize(img, (400, 200))
+    cv2.imshow("image", imS)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
