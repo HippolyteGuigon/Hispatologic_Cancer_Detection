ffcc0d77011d3376a4feea2c8861b5524ecde151
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2022-12-15 04:15:40 +0100

    model_first_iteration

--- src/visualisation/visualisation.py
+++ src/visualisation/visualisation.py
@@ -1,9 +1,10 @@
-import torch
 import cv2
 import os
 
 
-def image_visualisation(image="f0c2a0b8ef3024f407fa97d852d49be0215cafe0.tif",flag=1) -> None:
+def image_visualisation(
+    image="f0c2a0b8ef3024f407fa97d852d49be0215cafe0.tif", flag=-1
+) -> None:
     """
     The goal of this function is to display a given image
     for a certain time until the user hits a command
@@ -11,9 +12,9 @@ def image_visualisation(image="f0c2a0b8ef3024f407fa97d852d49be0215cafe0.tif",fla
     Arguments:
         -image: str: The name of the image the user wants to
         display
-        -flag: int : The way the image is loaded, how colours 
+        -flag: int : The way the image is loaded, how colours
         appear
-        
+
     Returns:
         None
     """
@@ -23,7 +24,7 @@ def image_visualisation(image="f0c2a0b8ef3024f407fa97d852d49be0215cafe0.tif",fla
     else:
         path = os.path.join("test", image)
 
-    img = cv2.imread(path,flag)
+    img = cv2.imread(path, flag)
     imS = cv2.resize(img, (400, 200))
     cv2.imshow("image", imS)
     cv2.waitKey(0)
